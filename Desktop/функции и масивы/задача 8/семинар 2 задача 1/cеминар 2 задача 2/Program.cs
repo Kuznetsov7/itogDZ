727fe16f1879323c47ffc54c51c1e3d06ffb1c7e
andrey <kuznetsov.1999.1999@gmail.com>
2022-07-18 20:10:10 +0300

    задача 2

---
+++ Desktop/функции и масивы/задача 8/семинар 2 задача 1/cеминар 2 задача 2/Program.cs
@@ -0,0 +1,15 @@
+﻿//Напишите программу ,которая выводит случайное трехзначное число и удаляет вторую цифру этого чисал
+
+int a = new Random().Next (100, 1000);
+Console.WriteLine (a);
+
+int result = deleteSecondNumber (a);
+Console.WriteLine (result);
+
+int deleteSecondNumber (int number)
+{
+    int digitOne = number / 100;
+    int digitThird = number % 10;
+    int resultDooble = digitOne * 10 + digitThird;
+    return resultDooble;
+}
