a4a12722be753f554f80c203b09c766fe50473a3
andrey <kuznetsov.1999.1999@gmail.com>
2022-07-18 19:35:59 +0300

    задfxf 1

---
+++ Desktop/функции и масивы/задача 8/семинар 2 задача 1/Program.cs
@@ -0,0 +1,34 @@
+﻿/* 
+Напишите программу,которая выдает случайные числа из отрезка [10, 99] и показывает цифру числа
+
+78 -> 8
+12 -> 2
+85 -> 8
+*/
+
+int num = new Random().Next(10, 100);
+Console.WriteLine("Random number: " + num);
+
+int firstDigit = num / 10;
+int secondDigit = num % 10;
+
+if (firstDigit > secondDigit)
+{
+    Console.WriteLine("Max number is " + firstDigit);
+}
+else
+{
+    Console.WriteLine("Max number is " + secondDigit);
+}
+
+int getMax(int a, int b)
+{
+ if (a > b)
+ {
+    return a;
+ }
+ else
+ {
+    return b;
+ }
+}
