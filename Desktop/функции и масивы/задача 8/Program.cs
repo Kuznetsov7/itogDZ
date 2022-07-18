b8b8edcb9f09894320f8c66d0701d3010c60ec65
andrey <kuznetsov.1999.1999@gmail.com>
2022-07-18 10:11:53 +0300

    задача 8

---
+++ Desktop/функции и масивы/задача 8/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("enter number: ");
+int num = int.Parse(Console.ReadLine());
+
+int i = 1;
+while (i <= num)
+{
+    if (i % 2 == 0)
+    {
+        Console.Write(i + " ");
+    }
+    i++;
+}
