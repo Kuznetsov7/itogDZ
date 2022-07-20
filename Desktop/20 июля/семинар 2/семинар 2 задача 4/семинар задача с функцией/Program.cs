8e2be163a376572314a3069023105a88e01bd370
andrey <kuznetsov.1999.1999@gmail.com>
2022-07-20 20:10:07 +0300

    задача tot

---
+++ Desktop/20 июля/семинар 2/семинар 2 задача 4/семинар задача с функцией/Program.cs
@@ -0,0 +1,18 @@
+﻿KratnoeChislo(161);
+KratnoeChislo(16);
+KratnoeChislo(41);
+
+
+void KratnoeChislo(int number)
+{
+    if (number % 7 == 0 && number % 23 == 0)
+    {
+        Console.WriteLine("Число " + number + " является кратным 7 и 23");
+    }
+    else
+    {
+        Console.WriteLine("Число " + number + " не является кратным 7 и 23");
+    }
+}
+
+
