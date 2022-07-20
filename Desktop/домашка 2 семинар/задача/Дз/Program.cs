1f9297df1ba9f6b4564970f0ba2d473cbe112793
andrey <kuznetsov.1999.1999@gmail.com>
2022-07-20 10:05:40 +0300

    задача 13

---
+++ Desktop/домашка 2 семинар/задача/Дз/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Enter number: ");
+int num = int.Parse(Console.ReadLine());
+
+if ( num < 100)
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+else
+{
+    while ( num > 999)
+    {
+        num = num / 10;
+    }
+    num = num % 10;
+    Console.WriteLine("Result: " + num);
+}
