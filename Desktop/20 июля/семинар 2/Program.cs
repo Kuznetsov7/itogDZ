c9cefb85fa4c8378b655614259ae541831101344
andrey <kuznetsov.1999.1999@gmail.com>
2022-07-20 19:41:30 +0300

    задача

---
+++ Desktop/20 июля/семинар 2/Program.cs
@@ -0,0 +1,11 @@
+﻿int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+
+if (b % a == 0) 
+{
+    Console.WriteLine("Число "+ b +" кратно числу"+ a);
+}
+else
+{
+    Console.WriteLine("Число "+ b +" не кратно числу "+ a +" , остаток от деления "+ (b % a));
+}
