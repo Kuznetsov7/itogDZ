4e70966eda18f7baa6e907490e5f0885916d92e8
andrey <kuznetsov.1999.1999@gmail.com>
2022-07-21 20:06:17 +0300

    задача tot3

---
+++ Desktop/20 июля/семинар 2/семинар 2 задача 4/семинар задача с функцией/семинар 2,третий час/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("Введите первое число");
+int First = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int Second = int.Parse(Console.ReadLine());
+
+if(IsSqr(First, Second))
+{
+    Console.WriteLine("yes");
+}
+else
+{
+    Console.WriteLine("no");
+}
+
+
+bool IsSqr (int num1, int num2)
+{
+    if(num1 * num1 == num2 || num2 * num2 == num1)
+{
+    return true;
+}
+else
+{
+    return false;
+}
+}
