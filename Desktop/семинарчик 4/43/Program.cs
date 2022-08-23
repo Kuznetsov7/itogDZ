c33bb6221c38564f1242b102d73c437d5b29d12d
andrey <kuznetsov.1999.1999@gmail.com>
2022-08-23 11:22:43 +0300

    задfxf 3

---
+++ Desktop/семинарчик 4/43/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Введите число: ");
+int NumberN = Convert.ToInt32(Console.ReadLine());
+int SumNumber(int NumberN)
+{ 
+  int count = Convert.ToString(NumberN).Length;
+  int a = 0;
+  int res = 0;
+  for (int i = 0; i < count; i++)
+  {
+    a = NumberN - NumberN % 10;
+    res = res + (NumberN - a);
+    NumberN = NumberN / 10;
+      }
+      return res;
+}
+int sumNumber = SumNumber(NumberN);
+Console.WriteLine("Сумма цифр: " + sumNumber);
