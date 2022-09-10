253208afd7d12f8eacecb7774c35f5fa72a3c5d0
andrey <kuznetsov.1999.1999@gmail.com>
2022-09-11 00:06:33 +0300

    удалил лишнюю переменную

--- Program.cs
+++ Program.cs
@@ -1,6 +1,5 @@
 ﻿string[] array1 = new string[5] { "123", "23", "hello", "world", "res" };
 string[] array2 = new string[array1.Length];
-int count = 0;
 void SecondArrayWithIF(string[] array1, string[] array2)
 {
     int count = 0;
