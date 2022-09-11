d29967c5b06906ee053e92d22ac421e2c0fb8427
andrey <kuznetsov.1999.1999@gmail.com>
2022-09-11 09:08:44 +0300

    исправил

--- Program.cs
+++ Program.cs
@@ -12,18 +12,7 @@ void SecondArrayWithIF(string[] array1, string[] array2)
         }
     }
 }
-void SecondArrayWithIF(string[] array1, string[] array2)
-{
-    int count = 0;
-    for (int i = 0; i < array1.Length; i++)
-    {
-        if (array1[i].Length <= 3)
-        {
-            array2[count] = array1[i];
-            count++;
-        }
-    }
-}
+
 void PrintArray(string[] array)
 {
     for (int i = 0; i < array.Length; i++)
